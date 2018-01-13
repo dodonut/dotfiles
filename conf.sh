c1412aede1618982acd0551ee9d458b9b1404364
root <root@localhost.localdomain>
2018-01-13 13:14:35 +0000

    finished

--- conf.sh
+++ conf.sh
@@ -1,8 +1,11 @@
 mkdir $HOME/.config/nvim
 cp init.vim $HOME/.config/nvim
 apt-get update
+apt-get install python3-pip -y
 apt-get install neovim -y
-./$HOME/.config/nvim/repos/github.com/junegunn/fzf/install -y -y -y
+pip3 install neovim
+cd $HOME/.config/nvim/repos/github.com/junegunn/fzf
+./install --all
 
 
 mkdir $HOME/Desktop/codes
@@ -10,3 +13,4 @@ cd $HOME/Desktop/codes
 git init
 git remote add origin http://github.com/toomanysand/codes
 git pull origin master
+nvim
