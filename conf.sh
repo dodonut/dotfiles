d6ca87b9cb0f200c391fdbb4e2a23ba5f56a3079
berserk <berserk@localhost.localdomain>
2018-10-31 11:37:10 +0000

    changed bash script

--- conf.sh
+++ conf.sh
@@ -1,18 +1,6 @@
 mkdir $HOME/.config/nvim
 cp init.vim $HOME/.config/nvim
-apt-get update
-apt-get install python3-pip -y
-apt-get install neovim -y
-pip3 install neovim
+sudo apt-get update
+sudo apt-get install python3-neovim python-neovim -y
 cd $HOME/.config/nvim/repos/github.com/junegunn/fzf
 ./install --all
-
-
-mkdir $HOME/Desktop/codes
-cd $HOME/Desktop/codes
-git init
-git remote add origin http://github.com/toomanysand/codes
-git pull origin master
-git config --global user.name "toomanysand"
-git config --global user.email "viniciusvims@gmail.com"
-nvim $HOME/Desktop/codes
