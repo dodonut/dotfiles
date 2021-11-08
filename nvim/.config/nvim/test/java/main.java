3a3c862c06a2f630a0d30a5b723c339828e04b13
dodonut <viniciusvims@gmail.com>
2021-11-08 09:52:49 -0300

    test commit

--- nvim/.config/nvim/test/java/main.java
+++ nvim/.config/nvim/test/java/main.java
@@ -6,6 +6,6 @@ import java.util.List;
 
 class HelloWorld {
     public static void main(String[] args) {
-        List<String> out = new ArrayList<>(1,2,3);
+        List<String> o = new ArrayList<>(1,2,3);
     }
 }
